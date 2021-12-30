68b42af55a246730b9ea701de49bb4c66202219c
sahil rana <74972430+sahil825@users.noreply.github.com>
2021-12-30 10:53:12 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,11 @@
 from django.urls import path,re_path
-from Users.views import SignUpController,SignInController,HomePageController,LogOutController
+from Bid.views import CreateController,DeleteController
 
 
 
-app_name='Users'
+app_name='Bid'
 urlpatterns = [
-    re_path(r'^sign-in/$', SignInController, name='sign-in'),
-    re_path(r'^sign-up/$', SignUpController, name='sign-up' ),
-    re_path(r'^home-page/$',  HomePageController, name='home-page'),
-    re_path(r'^log-out/$',  LogOutController, name='log-out'),
-
-    
+    re_path(r'^$', CreateController, name='create'),
+    re_path(r'^delete/$', DeleteController, name='delete' )
+        
     ]
